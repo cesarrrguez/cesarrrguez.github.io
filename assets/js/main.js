a450153fa5e771b15f081a58e730736feec931e8
César Rodríguez <cesar.rrguez@gmail.com>
2020-05-12 12:32:46 +0200

    Add header and hero section

--- assets/js/main.js
+++ assets/js/main.js
@@ -9,4 +9,25 @@
         $(this).remove();
       });
   });
+
+  // AOS initialization
+  AOS.init({
+    duration: 1000,
+    easing: 'ease-in-out',
+    once: true,
+    mirror: false,
+  });
+
+  // Toggle .header-scrolled class to #header when page is scrolled
+  $(window).scroll(function () {
+    if ($(this).scrollTop() > 100) {
+      $('#header').addClass('header-scrolled');
+    } else {
+      $('#header').removeClass('header-scrolled');
+    }
+  });
+
+  if ($(window).scrollTop() > 100) {
+    $('#header').addClass('header-scrolled');
+  }
 })(window.jQuery);
