ae16ceee2136eac83b6d08ec129fe3e918f3acf8
César Rodríguez <cesar.rrguez@gmail.com>
2020-05-14 09:53:09 +0200

    Add back to top button

--- assets/js/main.js
+++ assets/js/main.js
@@ -30,4 +30,24 @@
   if ($(window).scrollTop() > 100) {
     $('#header').addClass('header-scrolled');
   }
+
+  // Back to top button
+  $(window).scroll(function () {
+    if ($(this).scrollTop() > 100) {
+      $('.back-to-top').fadeIn('slow');
+    } else {
+      $('.back-to-top').fadeOut('slow');
+    }
+  });
+
+  $('.back-to-top').click(function () {
+    $('html, body').animate(
+      {
+        scrollTop: 0,
+      },
+      1500,
+      'easeInOutExpo'
+    );
+    return false;
+  });
 })(window.jQuery);
